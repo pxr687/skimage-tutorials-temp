39dbd67a56920f26f94da4c7412b5c9a276a9aca
Matthew Brett <matthew.brett@gmail.com>
2025-10-25 12:27:31 +0100

    Convert colour to color throughout.
    Also, do some code formatting to hints.py

--- random_colors.py
+++ random_colors.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def random_colour_array(n_iters, fontsize, figsize =(12, 7)):
+
+def random_color_array(n_iters, fontsize, figsize =(12, 7)):
     plt.figure(figsize=figsize)
     two_D_ones_and_zeros = np.array([[1, 0,], 
                                      [0, 1,]])
@@ -16,39 +17,39 @@ def random_colour_array(n_iters, fontsize, figsize =(12, 7)):
         plt.matshow(three_D_stack_array_altered_blend, fignum=0)
         plt.title(f"Blue (X3) = {randoms[2]} \n Green (X2) = {randoms[1]} \n Red (X1) = {randoms[0]}",
                  fontsize=fontsize)
-        
+
+
 def plot_int_float(figsize=(12, 10)):
     plt.figure(figsize=figsize)
     for i in np.arange(6):
         plt.subplot(2, 3, i+1)
         if i <= 2:
             max_intensity_val = 255
-            img_arr = np.array([[1, 0,], 
+            img_arr = np.array([[1, 0,],
                                 [0, 1,]])
             dtype="`uint8` dtype"
             which_channel = np.array([max_intensity_val * (i==0),
-                                    max_intensity_val * (i==1),
-                                    max_intensity_val * (i==2)])
+                                      max_intensity_val * (i==1),
+                                      max_intensity_val * (i==2)])
             plt.matshow(np.stack([img_arr * which_channel[0],
-                            img_arr * which_channel[1],
-                            img_arr * which_channel[2]], 
-                            axis=2),
-                            fignum=0)
+                                  img_arr * which_channel[1],
+                                  img_arr * which_channel[2]],
+                                 axis=2),
+                        fignum=0)
         else:
             max_intensity_val = 1.0
             img_arr = np.array([[0., 0.],
                                 [0., 0.]])
             dtype="`float64` dtype"
             which_channel = np.array([max_intensity_val * (i==3),
-                                    max_intensity_val * (i==4),
-                                    max_intensity_val * (i==5)])
-            
+                                      max_intensity_val * (i==4),
+                                      max_intensity_val * (i==5)])
+
             to_plot = np.stack([img_arr,
                                 img_arr, 
                                 img_arr], 
-                            axis=2)
+                               axis=2)
             to_plot[[0, 1], [0, 1], :] = which_channel
-            plt.matshow(to_plot,
-                        fignum=0)
+            plt.matshow(to_plot, fignum=0)
 
         plt.title(f"{dtype}; (Max Intensity = {max_intensity_val})\nRed (X1) = {which_channel[0]}\nGreen (X2) = {which_channel[1]}\nBlue (X3) = {which_channel[2]}");
